c1183a3955b4bbc4cb8a34896eb0aaf11080d144
feniaster <feniasteraprilia@gmail.com>
2023-06-30 17:53:42 +0700

    masuk data mahasiswa

--- app/src/main/java/com/feniasteraprilia/sqllite202102342/DBHelper.java
+++ app/src/main/java/com/feniasteraprilia/sqllite202102342/DBHelper.java
@@ -17,11 +17,13 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("create table users(username TEXT primary key, password TEXT)");
+        sqLiteDatabase.execSQL("create table biodata(nim TEXT primary key, nama TEXT, jeniskelamin TEXT, alamat TEXT, email TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("drop table if exists users");
+        sqLiteDatabase.execSQL("drop table if exists biodata");
     }
     public Boolean insertData(String username, String password){
         SQLiteDatabase db = this.getWritableDatabase();
@@ -52,5 +54,70 @@ public class DBHelper extends SQLiteOpenHelper {
             return true;
         else
             return false;
+    }
+    public Boolean insertDataMhs (String nim,String nama, String jeniskelamin, String alamat, String email){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put("nim",nim);
+        values.put("nama",nama);
+        values.put("jeniskelamin", jeniskelamin);
+        values.put("alamat", alamat);
+        values.put("email", email);
+        long result = db.insert("biodata", null,values);
+        if (result==1) return false;
+        else
+            return true;
+    }
+    public Boolean checkNim (String nim){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from biodata where nim=?", new String[] {nim});
+        if (cursor.getCount()>0)
+            return true;
+        else
+            return false;
+    }
+
+    public Cursor tampilDataMhs(){
+        SQLiteDatabase db= this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from biodata", null);
+        return cursor;
+    }
+
+    public boolean hapusDataMhs(String nim){
+        SQLiteDatabase db= this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("Select * from biodata where nim=?", new String[]{nim});
+        if (cursor.getCount()>0){
+            long result = db.delete("biodata", "nim=?", new String[]{nim});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public Boolean editDataMhs (String nim,String nama, String jeniskelamin, String alamat, String email){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put("nim",nim);
+        values.put("nama",nama);
+        values.put("jeniskelamin", jeniskelamin);
+        values.put("alamat", alamat);
+        values.put("email", email);
+        Cursor cursor = db.rawQuery("Select * from biodata where nim=?", new String[]{nim});
+        if (cursor.getCount()>0) {
+            long result = db.update("biodata", values, "nim=?", new String[]{nim});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
     }
 }
--- app/src/main/java/com/feniasteraprilia/sqllite202102342/MahasiswaActivity.java
+++ app/src/main/java/com/feniasteraprilia/sqllite202102342/MahasiswaActivity.java
@@ -1,4 +1,128 @@
 package com.feniasteraprilia.sqllite202102342;
 
-public class MahasiswaActivity {
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MahasiswaActivity extends AppCompatActivity {
+
+    EditText nim, nama, jeniskelamin, alamat, email;
+    String ni, nma, jk, almt, em;
+    Button simpan, tampil, hapus, edit;
+    DBHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mahasiswa);
+
+        nim = findViewById(R.id.edtnim);
+        nama = findViewById(R.id.edtnama);
+        jeniskelamin = findViewById(R.id.edtjk);
+        alamat = findViewById(R.id.edtalamat);
+        email = findViewById(R.id.edtemail);
+        simpan = findViewById(R.id.simpan);
+        tampil = findViewById(R.id.tampil);
+        hapus = findViewById(R.id.hapus);
+        edit = findViewById(R.id.edit);
+        db = new DBHelper(this);
+
+
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String ni = nim.getText().toString();
+                String nma = nama.getText().toString();
+                String jk = jeniskelamin.getText().toString();
+                String almt = alamat.getText().toString();
+                String em = email.getText().toString();
+
+                if (TextUtils.isEmpty(ni) || TextUtils.isEmpty(nma) || TextUtils.isEmpty(jk)
+                        || TextUtils.isEmpty(almt) || TextUtils.isEmpty(em)) {
+                    Toast.makeText(MahasiswaActivity.this, "Semua Field Wajib diIsi", Toast.LENGTH_LONG).show();
+                } else {
+                    Boolean checkkode = db.checkNim(ni);
+                    if (checkkode == false) {
+                        Boolean insert = db.insertDataMhs(ni, nma, jk, almt, em);
+                        if (insert == true) {
+                            Toast.makeText(MahasiswaActivity.this, "Data berhasil disimpan", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(MahasiswaActivity.this, "Data gagal disimpan", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } else {
+                        Toast.makeText(MahasiswaActivity.this, "Data Mahasiswa Sudah Ada", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        tampil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = db.tampilDataMhs();
+                if (res.getCount() == 0) {
+                    Toast.makeText(MahasiswaActivity.this, "Tidak ada Data", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while (res.moveToNext()) {
+                    buffer.append("Nim Mahasiswa : " + res.getString(0) + "\n");
+                    buffer.append("Nama Mahasiswa : " + res.getString(1) + "\n");
+                    buffer.append("Jenis Kelamin : " + res.getString(2) + "\n");
+                    buffer.append("Alamat : " + res.getString(3) + "\n");
+                    buffer.append("Email : " + res.getString(4) + "\n");
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(MahasiswaActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("Biodata Mahasiswa");
+                builder.setMessage(buffer.toString());
+                builder.show();
+            }
+        });
+
+        hapus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String kb = nim.getText().toString();
+                Boolean cekHapusData = db.hapusDataMhs(kb);
+                if (cekHapusData == true)
+                    Toast.makeText(MahasiswaActivity.this, "Data Terhapus", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MahasiswaActivity.this, "Data Tidak Ada", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String ni = nim.getText().toString();
+                String nma = nama.getText().toString();
+                String jk = jeniskelamin.getText().toString();
+                String almt = alamat.getText().toString();
+                String em = email.getText().toString();
+                if (TextUtils.isEmpty(ni) || TextUtils.isEmpty(nma) || TextUtils.isEmpty(jk)
+                        || TextUtils.isEmpty(almt) || TextUtils.isEmpty(em)) {
+                    Toast.makeText(MahasiswaActivity.this, "Semua Field Wajib Diisi", Toast.LENGTH_SHORT).show();
+                } else {
+                    Boolean edit = db.editDataMhs(ni, nma, jk, almt, em);
+                    if (edit == true) {
+                        Toast.makeText(MahasiswaActivity.this, "Data Berhasil Diedit", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MahasiswaActivity.this, "Data Gagal Diedit", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
 }
